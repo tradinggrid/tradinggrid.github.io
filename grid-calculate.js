26ae27710de094dfe495e5d35d36fb8dc9b377e8
Jeferson Cruz <dev.blackfishlabs@gmail.com>
2021-10-01 15:13:18 -0400

    add dynamic grid

--- grid-calculate.js
+++ grid-calculate.js
@@ -1,13 +1,21 @@
 // DOM Mappings
 let gridValues = document.getElementById('gridValues');
+// Fixed
 let oneGridsDiff = document.getElementById('oneGridsDiff');
 let oneGridsBuy = document.getElementById('oneGridsBuy');
 let oneGridsFree = document.getElementById('oneGridsFree');
 let oneGridsPercBuy = document.getElementById('oneGridsPercBuy');
 let oneGridsPercFree = document.getElementById('oneGridsPercFree');
 let oneGridsProfit = document.getElementById('oneGridsProfit');
+// Dynamic
+let oneDynamicGridsBuy = document.getElementById('oneDynamicGridsBuy');
+let oneDynamicGridsFree = document.getElementById('oneDynamicGridsFree');
+let oneDynamicGridsPercBuy = document.getElementById('oneDynamicGridsPercBuy');
+let oneDynamicGridsPercFree = document.getElementById('oneDynamicGridsPercFree');
+let oneDynamicGridsProfit = document.getElementById('oneDynamicGridsProfit');
+// Button
 let calcButton = document.getElementById('calcButton');
-// text fields
+// Fixed
 let domHighPrice = document.getElementById('domHighPrice');
 let domLowPrice = document.getElementById('domLowPrice');
 let domPrice = document.getElementById('domPrice');
@@ -15,6 +23,13 @@ let domQtyPerGrid = document.getElementById('domQtyPerGrid');
 let domTradingFee = document.getElementById('domTradingFee');
 let domGridAmmt = document.getElementById('domGridAmmt');
 let domPrecision = document.getElementById('domPrecision');
+// Dynamic
+let domIntervalProf = document.getElementById('domIntervalProf');
+let domDynamicPrice = document.getElementById('domDynamicPrice');
+let domDynamicQtyPerGrid = document.getElementById('domDynamicQtyPerGrid');
+let domDynamicTradingFee = document.getElementById('domDynamicTradingFee');
+let domDynamicGridAmmt = document.getElementById('domDynamicGridAmmt');
+let domDynamicPrecision = document.getElementById('domDynamicPrecision');
 
 // leave be
 let gridsArray = [];
@@ -33,17 +48,78 @@ let precision;
 const findGridPrices = (highPrice, lowPrice, gridsAmmt, precision) => {
     let gridPrice = (highPrice - lowPrice) / gridsAmmt;
     let gridStep = parseFloat(lowPrice);
+
     for (i = 0; i < gridsAmmt; i++) {
         gridStep += gridPrice;
         gridsArray.push(gridStep.toFixed(precision));
     }
+
+    gridsArray.sort();
 };
+
+const findDynamicGridPrices = (interval, price, gridsAmmt, precision) => {
+    let diff1 = parseFloat(interval);
+    let gridStep1 = parseFloat(price);
+
+    for (i = 0; i < gridsAmmt; i++) {
+        gridStep1 -= diff1;
+        gridsArray.push(gridStep1.toFixed(precision));
+    }
+
+    let diff = parseFloat(interval);
+    let gridStep = parseFloat(price);
+
+    for (i = 0; i < gridsAmmt; i++) {
+        gridStep += diff;
+        gridsArray.push(gridStep.toFixed(precision));
+    }
+
+    gridsArray.sort();
+}
 
 // figure out the fee for one side of the trade
 const findTradeFee = (fee, quantity, price) => {
     feeTotal = (quantity * price) * (fee / 100);
     oneTradeFee = feeTotal.toFixed(precision);
 };
+
+function addDynamic() {
+    // clear the results area before displaying results for multiple calcs w/o reload
+    oneDynamicGridsBuy.innerText = '';
+    oneDynamicGridsFree.innerText = '';
+    oneDynamicGridsPercBuy.innerText = '';
+    oneDynamicGridsPercFree.innerText = '';
+    oneDynamicGridsProfit.innerText = '';
+
+    gridsArray = [];
+    gridValues.innerHTML = '';
+
+    interval = domIntervalProf.value;
+    price = domDynamicPrice.value;
+    quantity = domDynamicQtyPerGrid.value;
+    fee = domDynamicTradingFee.value;
+    gridsAmmt = domDynamicGridAmmt.value * 2;
+    precision = domDynamicPrecision.value;
+
+    findDynamicGridPrices(interval, price, gridsAmmt, precision);
+    findTradeFee(fee, quantity, gridsArray[0]);
+
+    var percentBuy = 50;
+    let percentFree = 50;
+
+    let oneGridProfit = (gridsArray[1] - gridsArray[0]) * quantity - oneTradeFee;
+    let oneGridBuy = ((quantity * price) * gridsAmmt);
+    let oneGridFree = ((quantity * price) * gridsAmmt);
+
+    oneDynamicGridsProfit.value = parseFloat(oneGridProfit).toFixed(precision);
+    oneDynamicGridsBuy.value = parseFloat(oneGridBuy).toFixed(precision);
+    oneDynamicGridsFree.value = parseFloat(oneGridFree).toFixed(precision);
+
+    oneDynamicGridsPercBuy.value = parseFloat(percentBuy).toFixed(0);
+    oneDynamicGridsPercFree.value = parseFloat(percentFree).toFixed(0);
+
+    gridsArray.forEach(aGrid => newElement(aGrid));
+}
 
 function add() {
     // clear the results area before displaying results for multiple calcs w/o reload
@@ -53,6 +129,7 @@ function add() {
     oneGridsPercBuy.innerText = '';
     oneGridsPercFree.innerText = '';
     oneGridsProfit.innerText = '';
+
     gridsArray = [];
     gridValues.innerHTML = '';
 
